268d1d7629b677035b2514bb72a4e898fb8fbc03
Tahmid Sharif <tahmidsharif@ids.company>
2020-08-31 16:24:03 +0600

    subscribe to newsletter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,31 @@
+<script>
+  import { goto, stores } from "@sapper/app";
+  import { onMount } from "svelte";
+  const { preloading, page, session } = stores();
+
+    const { host, path, params, query } = $page;
+
+  let sentConfirmation = false;
+
+  let email = "";
+
+  async function addToEmailList() {
+    const ref = query['ref'];
+    const res = await fetch('http://127.0.0.1:1337/subscribers', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        ref
+      })
+    });
+
+    if (res.status === 200) {
+      await goto('subscribe/check-mail');
+    }
+  }
+
+</script>
+
 <div
   class="mx-4 px-6 md:px-0 md:w-9/12 lg:w-7/12 xl:w-6/12 md:mx-auto my-12
   bg-pink-200 rounded py-12 lowercase shadow-md">
@@ -10,25 +38,38 @@
     </span>
   </div>
 
-	  <div class="mt-4 max-w-lg mx-auto">
-    <h1 class="font-bold text-3xl">want to find your new favourite
-band?</h1>
+  <div class="mt-4 max-w-lg mx-auto">
+    <h1 class="font-bold text-3xl">want to find your new favourite band?</h1>
   </div>
 
   <div class="mt-8 max-w-lg mx-auto text-xl font-semibold">
     <p class="text-xl">
-join the hot weekly newsletter for underground
-music lovers, finding the best new music one song
-at a time
+      join the hot weekly newsletter for underground music lovers, finding the
+      best new music one song at a time
     </p>
-		<div class="flex mt-8">
-			<input type="email" class="p-2 rounded-md border-4 border-white flex-auto bg-transparent" placeholder="email">
-			<button class="bg-white hover:bg-gray-200 shadow-md rounded-md px-4 py-2">try it</button>
-		</div>
-  </div>
-    <div class="mt-8 max-w-lg mx-auto flex justify-between text-xl text-gray-600 font-semibold">
-      <button href="/" class="bg-white hover:bg-gray-200 shadow-md rounded-md px-4 py-2">subscribe with facebook</button>
-      <button href="/" class="bg-white hover:bg-gray-200 shadow-md rounded-md px-4 py-2">subscribe with google</button>
+    <div class="flex mt-8">
+      <input
+        type="email"
+        class="p-2 rounded-md border-4 border-white flex-auto bg-transparent"
+        placeholder="email" bind:value={email}/>
+      <button class="bg-white hover:bg-gray-200 shadow-md rounded-md px-4 py-2" on:click={addToEmailList}>
+        try it
+      </button>
     </div>
+  </div>
+  <div
+    class="mt-8 max-w-lg mx-auto flex justify-between text-xl text-gray-600
+    font-semibold">
+    <button
+      href="/"
+      class="bg-white hover:bg-gray-200 shadow-md rounded-md px-4 py-2">
+      subscribe with facebook
+    </button>
+    <button
+      href="/"
+      class="bg-white hover:bg-gray-200 shadow-md rounded-md px-4 py-2">
+      subscribe with google
+    </button>
+  </div>
 
 </div>
