23a9db6b028da1f976ed64529fd7049c730b2984
Tahmid Sharif <tahmidsharif@ids.company>
2020-09-01 15:59:18 +0600

    add menu item

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -16,4 +16,10 @@
 
 <main>
 	<slot></slot>
+	<div class="mx-4 px-6 md:px-0 md:w-9/12 lg:w-7/12 xl:w-6/12 md:mx-auto
+   rounded lowercase flex justify-end mb-24">
+		<nav class="p-4 bg-pink-200 text-gray-700 rounded-lg ml-4 shadow-md font-semibold"><a href="/shop">shop</a></nav>
+		<nav class="p-4 bg-pink-200 text-gray-700 rounded-lg ml-4 shadow-md font-semibold"><a href="/submit-song">submit song</a></nav>
+		<nav class="p-4 bg-pink-200 text-gray-700 rounded-lg ml-4 shadow-md font-semibold"><a href="/privacy-policy">privacy policy</a></nav>
+	</div>
 </main>
