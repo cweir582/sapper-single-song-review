8c6f87d4ae775f3075856ab253648b4eca1a4009
Tahmid Sharif <tahmidsharif@ids.company>
2020-09-06 17:58:34 +0600

    show confirmation page

--- src/routes/checkout/confirmed.svelte
+++ src/routes/checkout/confirmed.svelte
@@ -3,27 +3,47 @@
   import { goto, stores } from "@sapper/app";
   const { preloading, page, session } = stores();
   const { host, path, params, query } = $page;
-  const { slug } = params;
+
+  let state = 'normal';
+  $: currentState = state;
+
 
   async function confirmOrder() {
     const res = await fetch("http://127.0.0.1:1337/orders/confirm/" + query['confirm']);
 
     if (res.status === 200) {
       const data = await res.json();
-      console.log(data);
-      if(data.order_status === 'paid') {
+      if(data.order_state === 'paid') {
         localStorage.removeItem('cart');
+        state = 'confirmed';
+      }else {
+        state = 'not confirmed';
       }
+    }else {
+      state = 'not confirmed';
     }
   }
 
   onMount(async () => {
-    await confirmOrder();
+    try {
+      await confirmOrder();
+    } catch (error) {
+      console.log(error);
+      state = 'wrong';
+    }
   });
 </script>
 
 <div
-  class="mx-4 px-6 md:px-0 md:w-9/12 lg:w-7/12 xl:w-6/12 md:mx-auto my-12
-  bg-pink-200 rounded py-12 lowercase shadow-md">
-  Confirm
+  class="mx-4 p-6 md:w-9/12 lg:w-7/12 xl:w-6/12 md:mx-auto my-12
+  bg-pink-200 rounded py-12 lowercase shadow-md font-semibold text-3xl">
+  
+  {#if currentState === 'confirmed'}
+    Your order is confirmed.
+  {:else if currentState === 'normal'}
+    Loading
+  {:else}
+    Something went wrong
+  {/if}
+
 </div>
